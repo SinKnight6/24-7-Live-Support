2224be8207e1e0c5485231d2a061707b10c998f0
SinKnight6 <sinnersquadhq@gmail.com>
2020-04-18 23:28:27 -0500

    third commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ bot.on('message', async message => {
 
 // Brealk 
 
-    if (message.content.toLowerCase === prefix + "live chat") {
+    if (message.content.toLowerCase === (prefix + "live chat")) {
       let uEmbed1 = new Discord.MessageEmbed()
       .setColor(0x00FF44)
       .setAuthor(`Hello ${message.author.username},`)
